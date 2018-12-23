ddfc6d1339dab8e80e4890e0a0a100786f38e08a
Kim Hyung Gyu <khkpoo@gmail.com>
2018-12-23 13:24:32 +0900

    test

--- TJ2ndProject/ViewController.swift
+++ TJ2ndProject/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnDown(_ sender: Any) {
+        lbl.text = "Seccessed"
+//        let test : UIView? = lbl.superview
+//        print(test?.textInputContextIdentifier)
+//        test?.backgroundColor = UIColor.green
+//        print(test?.accessibilityElementCount())
+//        test?.touchesBegan(<#T##touches: Set<UITouch>##Set<UITouch>#>, with: <#T##UIEvent?#>)
+        lbl.frame = CGRect.init(x: 10, y: 10, width: 30, height: 30)
+        lbl.frame.origin = CGPoint.init(x: 30, y: 30)
+        lbl.frame.size = CGSize.init(width: 100, height: 100)
+    }
+    
 }
 
